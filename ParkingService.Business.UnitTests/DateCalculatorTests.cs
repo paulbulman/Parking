0418f8bc86e1a961f59ed95b150dcfbfc96cb90c
Paul Bulman <paulbulman@gmail.com>
2020-09-05 17:37:17 +0100

    Add lead time calculations

--- ParkingService.Business.UnitTests/DateCalculatorTests.cs
+++ ParkingService.Business.UnitTests/DateCalculatorTests.cs
@@ -56,6 +56,106 @@ namespace ParkingService.Business.UnitTests
             Assert.Equal(1.July(2020), summerResult.First());
         }
 
+        [Fact]
+        public void GetShortLeadTimeAllocationDates_returns_current_working_day_if_called_before_11_am()
+        {
+            var instant = 4.September(2020).At(10, 59, 59).InZoneStrictly(LondonTimeZone).ToInstant();
+            
+            var result = CreateDateCalculator(instant).GetShortLeadTimeAllocationDates();
+
+            Assert.Single(result);
+            Assert.Equal(4.September(2020), result.Single());
+        }
+
+        [Fact]
+        public void GetShortLeadTimeAllocationDates_returns_current_and_next_working_days_if_called_after_11_am()
+        {
+            var instant = 4.September(2020).At(11, 0, 0).InZoneStrictly(LondonTimeZone).ToInstant(); 
+
+            var result = CreateDateCalculator(instant).GetShortLeadTimeAllocationDates();
+
+            Assert.Equal(2, result.Count);
+            Assert.Equal(4.September(2020), result.First());
+            Assert.Equal(7.September(2020), result.Last());
+        }
+
+        [Fact]
+        public void GetShortLeadTimeAllocationDates_uses_London_time_zone()
+        {
+            var winterInstant = 31.January(2020).At(10, 0, 0).Utc();
+            var winterResult = CreateDateCalculator(winterInstant).GetShortLeadTimeAllocationDates();
+            Assert.Equal(31.January(2020), winterResult.Last());
+
+            var summerInstant = 30.June(2020).At(10, 0, 0).Utc();
+            var summerResult = CreateDateCalculator(summerInstant).GetShortLeadTimeAllocationDates();
+            Assert.Equal(1.July(2020), summerResult.Last());
+        }
+
+        [Fact]
+        public void GetShortLeadTimeAllocationDates_returns_next_working_day_if_called_at_weekend()
+        {
+            var instant = 5.September(2020).At(11, 0, 0).InZoneStrictly(LondonTimeZone).ToInstant();
+
+            var result = CreateDateCalculator(instant).GetShortLeadTimeAllocationDates();
+
+            Assert.Single(result);
+            Assert.Equal(7.September(2020), result.Single());
+        }
+
+        [Fact]
+        public void GetShortLeadTimeAllocationDates_returns_next_working_day_if_called_on_bank_holiday()
+        {
+            var bankHoliday = 31.August(2020);
+            var instant = bankHoliday.At(11, 0, 0).InZoneStrictly(LondonTimeZone).ToInstant();
+
+            var result = CreateDateCalculator(instant, bankHoliday).GetShortLeadTimeAllocationDates();
+
+            Assert.Single(result);
+            Assert.Equal(1.September(2020), result.Single());
+        }
+
+        [Fact]
+        public void GetLongLeadTimeAllocationDates_returns_date_after_short_lead_time_period_first()
+        {
+            var result = CreateDateCalculator(7.September(2020)).GetLongLeadTimeAllocationDates();
+            Assert.Equal(8.September(2020), result.First());
+        }
+
+        [Fact]
+        public void GetLongLeadTimeAllocationDates_returns_date_at_end_of_next_week_last()
+        {
+            var result = CreateDateCalculator(7.September(2020)).GetLongLeadTimeAllocationDates();
+            Assert.Equal(18.September(2020), result.Last());
+        }
+
+        [Fact]
+        public void GetLongLeadTimeAllocationDates_rolls_over_to_new_week_on_Thursdays()
+        {
+            // On a Wednesday, the last date should be the Friday 9 days later.
+            var wednesdayResult = CreateDateCalculator(9.September(2020)).GetLongLeadTimeAllocationDates();
+            Assert.Equal(10.September(2020), wednesdayResult.First());
+            Assert.Equal(18.September(2020), wednesdayResult.Last());
+
+            // On a Thursday, the last date should be the Friday 15 days later.
+            var thursdayResult = CreateDateCalculator(10.September(2020)).GetLongLeadTimeAllocationDates();
+            Assert.Equal(11.September(2020), thursdayResult.First());
+            Assert.Equal(25.September(2020), thursdayResult.Last());
+        }
+
+        [Fact]
+        public void GetLongLeadTimeAllocationDates_uses_London_time_zone()
+        {
+            // This is still Wednesday local time, so should only includes the current and next weeks.
+            var winterInstant = 1.January(2020).At(23, 0, 0).Utc();
+            var winterResult = CreateDateCalculator(winterInstant).GetLongLeadTimeAllocationDates();
+            Assert.Equal(10.January(2020), winterResult.Last());
+
+            // This is Thursday local time, so should include the current, next and one subsequent week.
+            var summerInstant = 3.June(2020).At(23, 0, 0).Utc();
+            var summerResult = CreateDateCalculator(summerInstant).GetLongLeadTimeAllocationDates();
+            Assert.Equal(19.June(2020), summerResult.Last());
+        }
+
         private static DateCalculator CreateDateCalculator(LocalDate londonDate, params LocalDate[] bankHolidayDates)
         {
             var londonMidnight = londonDate.AtMidnight().InZoneStrictly(LondonTimeZone).ToInstant();
--- ParkingService.Business/DateCalculator.cs
+++ ParkingService.Business/DateCalculator.cs
@@ -27,14 +27,51 @@
             var firstDayOfSubsequentMonth = firstDayOfThisMonth.PlusMonths(2);
             var lastDayOfNextMonth = firstDayOfSubsequentMonth.PlusDays(-1);
 
-            return this.DatesBetween(currentDate, lastDayOfNextMonth);
+            return this.WorkingDatesBetween(currentDate, lastDayOfNextMonth);
         }
+
+        public IReadOnlyCollection<LocalDate> GetShortLeadTimeAllocationDates()
+        {
+            var currentTime = this.GetCurrentTime();
+
+            var firstDate = this.GetNextWorkingDayIncluding(currentTime.Date);
+
+            var lastDate = firstDate == currentTime.Date && currentTime.Hour >= 11 ?
+                this.GetNextWorkingDayStrictlyAfter(firstDate) :
+                firstDate;
+
+            return new[] { firstDate, lastDate }.Distinct().ToList();
+        }
+
+        public IReadOnlyCollection<LocalDate> GetLongLeadTimeAllocationDates()
+        {
+            var lastShortLeadTimeAllocationDate = this.GetShortLeadTimeAllocationDates().Last();
+
+            var firstDate = this.GetNextWorkingDayStrictlyAfter(lastShortLeadTimeAllocationDate);
+
+            var lastDate = GetCurrentDate().Next(IsoDayOfWeek.Thursday).PlusWeeks(1).PlusDays(1);
+
+            return this.WorkingDatesBetween(firstDate, lastDate);
+        }
+
+        private LocalDate GetNextWorkingDayIncluding(LocalDate localDate)
+        {
+            while (!this.IsWorkingDay(localDate))
+            {
+                localDate = localDate.PlusDays(1);
+            }
+
+            return localDate;
+        }
+
+        private LocalDate GetNextWorkingDayStrictlyAfter(LocalDate localDate) =>
+            this.GetNextWorkingDayIncluding(localDate.PlusDays(1));
 
         private LocalDate GetCurrentDate() => this.GetCurrentTime().Date;
 
         private ZonedDateTime GetCurrentTime() => this.CurrentInstant.InZone(LondonTimeZone);
 
-        private IReadOnlyCollection<LocalDate> DatesBetween(LocalDate firstDate, LocalDate lastDate) =>
+        private IReadOnlyCollection<LocalDate> WorkingDatesBetween(LocalDate firstDate, LocalDate lastDate) =>
             Enumerable.Range(0, Period.Between(firstDate, lastDate, PeriodUnits.Days).Days + 1)
                 .Select(firstDate.PlusDays)
                 .Where(this.IsWorkingDay)
