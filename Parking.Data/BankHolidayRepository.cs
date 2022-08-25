443baad0fd1bf648beb786d0c2fbf887a8e09835
Paul Bulman <paulbulman@gmail.com>
2022-08-25 19:46:45 +0100

    Update bank holidays

--- Parking.Data/BankHolidayRepository.cs
+++ Parking.Data/BankHolidayRepository.cs
@@ -10,13 +10,6 @@
         public IReadOnlyCollection<BankHoliday> GetBankHolidays() =>
             new[]
             {
-                new BankHoliday(new LocalDate(2021, 4, 2)),
-                new BankHoliday(new LocalDate(2021, 4, 5)),
-                new BankHoliday(new LocalDate(2021, 5, 3)),
-                new BankHoliday(new LocalDate(2021, 5, 31)),
-                new BankHoliday(new LocalDate(2021, 8, 30)),
-                new BankHoliday(new LocalDate(2021, 12, 27)),
-                new BankHoliday(new LocalDate(2021, 12, 28)),
                 new BankHoliday(new LocalDate(2022, 1, 3)),
                 new BankHoliday(new LocalDate(2022, 4, 15)),
                 new BankHoliday(new LocalDate(2022, 4, 18)),
@@ -25,7 +18,15 @@
                 new BankHoliday(new LocalDate(2022, 6, 3)),
                 new BankHoliday(new LocalDate(2022, 8, 29)),
                 new BankHoliday(new LocalDate(2022, 12, 26)),
-                new BankHoliday(new LocalDate(2022, 12, 27))
+                new BankHoliday(new LocalDate(2022, 12, 27)),
+                new BankHoliday(new LocalDate(2023, 1, 2)),
+                new BankHoliday(new LocalDate(2023, 4, 7)),
+                new BankHoliday(new LocalDate(2023, 4, 10)),
+                new BankHoliday(new LocalDate(2023, 5, 1)),
+                new BankHoliday(new LocalDate(2023, 5, 29)),
+                new BankHoliday(new LocalDate(2023, 8, 28)),
+                new BankHoliday(new LocalDate(2023, 12, 25)),
+                new BankHoliday(new LocalDate(2023, 12, 26)),
             };
     }
 }
