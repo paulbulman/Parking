48a9199375d69a7f89d93ef4f782140e68f17c2f
Paul Bulman <paulbulman@gmail.com>
2023-04-25 16:41:28 +0100

    Attempt to work around strange LocalStack(?) behaviour

--- Parking.TestHelpers/Aws/StorageHelpers.cs
+++ Parking.TestHelpers/Aws/StorageHelpers.cs
@@ -2,7 +2,6 @@
 {
     using System.Collections.Generic;
     using System.IO;
-    using System.Linq;
     using System.Threading.Tasks;
     using Amazon.Runtime;
     using Amazon.S3;
@@ -26,7 +25,7 @@
         {
             using var client = CreateClient();
 
-            await DeleteBuckets(client);
+            await DeleteEmailBucketIfExists(client);
 
             await client.PutBucketAsync(EmailBucketName);
         }
@@ -35,50 +34,50 @@
         {
             using var client = CreateClient();
 
-            var contents = await GetBucketContents(client, EmailBucketName);
+            var contents = await GetEmailBucketContents(client);
 
             var result = new List<string>();
 
             foreach (var s3Object in contents.S3Objects)
             {
-                result.Add(await GetFileContent(client, s3Object));
+                result.Add(await GetEmailFileContent(client, s3Object));
             }
 
             return result;
         }
 
-        private static async Task DeleteBuckets(IAmazonS3 client)
+        private static async Task DeleteEmailBucketIfExists(IAmazonS3 client)
         {
             var bucketsResponse = await client.ListBucketsAsync();
 
-            foreach (var bucketName in bucketsResponse.Buckets.Select(b => b.BucketName))
+            if (bucketsResponse.Buckets.Exists(b => b.BucketName == EmailBucketName))
             {
-                await DeleteBucket(client, bucketName);
+                await DeleteEmailBucket(client);
             }
         }
 
-        private static async Task DeleteBucket(IAmazonS3 client, string bucketName)
+        private static async Task DeleteEmailBucket(IAmazonS3 client)
         {
-            var contents = await GetBucketContents(client, bucketName);
+            var contents = await GetEmailBucketContents(client);
 
             foreach (var contentsS3Object in contents.S3Objects)
             {
-                await client.DeleteObjectAsync(contentsS3Object.BucketName, contentsS3Object.Key);
+                await client.DeleteObjectAsync(EmailBucketName, contentsS3Object.Key);
             }
 
-            await client.DeleteBucketAsync(bucketName);
+            await client.DeleteBucketAsync(EmailBucketName);
         }
 
-        private static async Task<ListObjectsV2Response> GetBucketContents(IAmazonS3 client, string bucketName)
+        private static async Task<ListObjectsV2Response> GetEmailBucketContents(IAmazonS3 client)
         {
-            var listObjectsRequest = new ListObjectsV2Request { BucketName = bucketName };
+            var listObjectsRequest = new ListObjectsV2Request { BucketName = EmailBucketName };
 
             return await client.ListObjectsV2Async(listObjectsRequest);
         }
 
-        private static async Task<string> GetFileContent(IAmazonS3 client, S3Object s3Object)
+        private static async Task<string> GetEmailFileContent(IAmazonS3 client, S3Object s3Object)
         {
-            var response = await client.GetObjectAsync(s3Object.BucketName, s3Object.Key);
+            var response = await client.GetObjectAsync(EmailBucketName, s3Object.Key);
 
             await using var responseStream = response.ResponseStream;
 
